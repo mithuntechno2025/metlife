73f9f31b8239f6ab18b740f13ad7b8c9ef0d1a6f
rahul <ec2-user@ip-172-31-36-15.ap-south-1.compute.internal>
2025-09-27 17:18:13 +0000

    Use cherry-pick command

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ This is the deployment file
 Added some more deplpyment stratergies
 Pipeline updated from master team
 Added the file regarding the new release from dev team
-
+Use of cherry-pick command
